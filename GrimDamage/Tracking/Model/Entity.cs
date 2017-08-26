787b379b508ae2ecc385c02f46f7705ffa7eacf1
marius <e@ma.il>
2017-08-26 13:16:35 +0200

    -

--- GrimDamage/Tracking/Model/Entity.cs
+++ GrimDamage/Tracking/Model/Entity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace GrimDawnTestListener.Tracking.Model {
     class Entity {
         public bool IsPlayer { get; set; }
-        public List<DamageType> DamageDealt { get; set; }
-        public List<DamageType> DamageTaken { get; set; }
+        public List<DamageEntry> DamageDealt { get; set; }
+        public List<DamageEntry> DamageTaken { get; set; }
     }
 }
