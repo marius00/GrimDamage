2feb84007aaf1b1c123f55a8a487f17305476e52
marius <e@ma.il>
2017-09-04 20:06:32 +0200

    Dynamic data added

--- GrimDamage/Content/js/integration.js
+++ GrimDamage/Content/js/integration.js
@@ -6,8 +6,20 @@ function _itemsReceived() {
         JSON.parse(data.damageTakenJson));
 }
 
+// TODO: move this to main.js
 function itemsReceived(players, damageDealt, damageTaken) {
-    console.log(players, damageDealt, damageTaken);
+    if (players.length == 0) {
+        console.log("No player yet.. skipping graph..");
+    } else {
+        var id = players[0].id;
+        for (var i = 0; i < damageDealt[id].length; i++) {
+            console.log("Adding ", damageDealt[id][i].amount, ' to ', damageDealt[id][i].damageType);
+            globalChart.series.filter(s => s.name === damageDealt[id][i].damageType)[0].addPoint(damageDealt[id][i].amount, true, true);
+        }
+        
+    }
+
+    
 }
 
 var intervalID = window.setInterval(data.requestUpdate, 1000);
