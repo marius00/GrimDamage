d9d792bbad26a8321ba3120d0b97c100a6fa1f17
marius <e@ma.il>
2017-09-04 19:46:33 +0200

    Added highcharts

---
+++ GrimDamage/Content/js/integration.js
@@ -0,0 +1,13 @@
+﻿
+function _itemsReceived() {
+    itemsReceived(
+        JSON.parse(data.playersJson),
+        JSON.parse(data.damageDealtJson),
+        JSON.parse(data.damageTakenJson));
+}
+
+function itemsReceived(players, damageDealt, damageTaken) {
+    console.log(players, damageDealt, damageTaken);
+}
+
+var intervalID = window.setInterval(data.requestUpdate, 1000);
