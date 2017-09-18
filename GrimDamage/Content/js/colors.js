b3cd458d6a12248d87e2c5a7713632a80b71950f
Ola Albertsson <ola.albertsson@aloq.se>
2017-09-18 08:25:10 +0200

    Started on colors

---
+++ GrimDamage/Content/js/colors.js
@@ -0,0 +1,21 @@
+﻿class Colors {
+
+    construction() {
+        this.physical = '#000000';
+        this.internalTrauma = '#4a4a4a';
+        this.fire = '#ff6300';
+        this.burn = '#9e3d00';
+        this.cold = '#00beff';
+        this.frostburn = '#0088b6';
+        this.lightning = '#00fffe';
+        this.electrocute = '#00bfbe';
+        this.acid = '#26ff00';
+        this.poison = '#179a00';
+        this.vitality = '#fcff00';
+        this.vitalityDecay = '#969800';
+        this.pierce = '#ff009f';
+        this.bleeding = '#ff0000';
+        this.aether = '#89ffbb';
+        this.chaos = '#720b00';
+    }
+}
