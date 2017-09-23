04d5072d685e2a3852327fa9ffa6dbb6cc4e3263
marius <e@ma.il>
2017-09-23 11:09:14 +0200

    Working on dashboard integration

---
+++ GrimDamage/Content/js/charts/damage-taken-pie.js
@@ -0,0 +1,59 @@
+﻿function createDamageTakenPieChart(id) {
+    return Highcharts.chart(id, {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        
+        title: {
+            text: null
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [
+                {
+                    name: 'Chaos',
+                    y: 56.33
+                },
+                {
+                    name: 'Fire',
+                    y: 24.03,
+                    sliced: true,
+                    selected: true
+                },
+                {
+                    name: 'Aether',
+                    y: 10.38
+                },
+                {
+                    name: 'Cold',
+                    y: 4.77
+                },
+                {
+                    name: 'Physical',
+                    y: 0.91
+                },
+                {
+                    name: 'Piercing',
+                    y: 0.2
+                }
+            ]
+        }]
+    });
+}
--- GrimDamage/Content/js/light-mode-toggle.js
+++ GrimDamage/Content/js/light-mode-toggle.js
@@ -7,14 +7,17 @@ class LightModeToggleViewModel {
     
         this.toggleDarkMode = () => {
             if (self.enabled()) {
+                console.log('Loading "Light mode"');
                 setCsharpLightMode('light');
                 document.location = 'index.html';
             } else {
+                console.log('Loading "Dark mode"');
                 setCsharpLightMode('dark');
                 document.location = 'index.html?DarkMode=1';
             }
         }
 
         this.label = ko.observable(enabled ? 'Light mode' : 'Dark mode');
+        console.log(`Initialize as ${enabled ? 'Dark mode' : 'Light mode'}`);
     }
 };
